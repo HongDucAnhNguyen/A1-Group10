071b7d6e20f836be6d7262f5532ed01c9d92da1a
simon <simon@dumalski.com>
2022-09-20 10:39:47 -0600

    added a way to read through the resource files

--- src/Driver.java
+++ src/Driver.java
@@ -1,7 +1,40 @@
+import shapes.Shape;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Driver {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //Load the file and check that it exists
+        File file = new File("../res/polyfor1.txt");
+        //File file = new File("../res/polyfor3.txt");
+        //File file = new File("../res/polyfor5.txt");
+
+        if (!file.exists()) {
+            System.out.println("Specified file does not exist.");
+            System.exit(0);
+        }
+
+        //Create a scanner for the file and set the delimiter to be a white space character
+        Scanner scanner = new Scanner(file);
+        scanner.useDelimiter(" ");
+
+        //Get the size of the array
+        int arraySize = scanner.nextInt();
+
+        //Create the array with the specified size
+        Shape[] shapes = new Shape[arraySize];
+
+        //Loop through the file
+        while (scanner.hasNext()) {
+
+
+
+        }
+
     }
 
 }
