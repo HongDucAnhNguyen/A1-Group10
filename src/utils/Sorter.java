85c9f5c8142839ba25baa7d9f1fbd80c7cd81f2f
Jay Nguyen <bobguy0606@gmail.com>
2022-09-20 15:30:45 -0700

    new utility class with sorting methods

---
+++ src/utils/Sorter.java
@@ -0,0 +1,26 @@
+package utils;
+
+public class Sorter {
+    public static void quickSort() {
+
+    }
+
+    public static void BubbleSort() {
+
+    }
+
+    public static void SelectionSort() {
+
+    }
+
+    public static void InsertionSort() {
+
+    }
+
+    public static void mergeSort() {
+    }
+
+    public static void SixthSort() {
+
+    }
+}
