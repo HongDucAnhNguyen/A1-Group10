40db7e806c6a9ca5f7982f886f5ee92025c7c198
Jay Nguyen <bobguy0606@gmail.com>
2022-09-23 15:36:15 -0700

    successfully implemented sorting logic to Collection of comparable objects

---
+++ src/shapes/CompareBaseArea.java
@@ -0,0 +1,11 @@
+package shapes;
+
+import java.util.Comparator;
+
+public class CompareBaseArea implements Comparator<GeneralShape> {
+
+    @Override
+    public int compare(GeneralShape o1, GeneralShape o2) {
+        return 0;
+    }
+}
---
+++ src/shapes/CompareVolume.java
@@ -0,0 +1,12 @@
+package shapes;
+
+
+import java.util.Comparator;
+
+public class CompareVolume implements Comparator<GeneralShape> {
+
+    @Override
+    public int compare(GeneralShape o1, GeneralShape o2) {
+        return 0;
+    }
+}
