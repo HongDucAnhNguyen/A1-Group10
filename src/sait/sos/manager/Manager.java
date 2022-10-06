dec0e051c1a6fafd11d585b6b6ea782aec7021ca
simondumalski <simon@dumalski.com>
2022-10-06 13:23:48 -0600

    Made the file object ternary operator even better (I think) and added a comment explaining its purpose

--- src/sait/sos/manager/Manager.java
+++ src/sait/sos/manager/Manager.java
@@ -85,7 +85,10 @@ public class Manager {
         double otherVal;
         int arrayIndex = 0;
 
-        Scanner inFile = new Scanner(new File(fileName.contains("res\\") || fileName.contains("res/") ? fileName : "res" + File.separator + fileName));
+        //The ternary operator here does the following:
+        //  1. Checks if the file already has "res" in it, adding it if it does
+        //  2. Changes "/" or "\" to be a system independent file separator so that it will work on Windows & macOS
+        Scanner inFile = new Scanner(new File(fileName.contains("res\\") ? fileName.replace("\\", File.separator) : fileName.contains("res/") ? fileName.replace("/", File.separator) : "res" + File.separator + fileName));
         inFile.useDelimiter(" ");
         
         int shapeSize = inFile.nextInt();
